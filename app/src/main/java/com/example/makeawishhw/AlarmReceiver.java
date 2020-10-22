ec8c01f1dc3a67f01a481391bbc1d0b89f08bce6
Matthew Mullikin <clemsongrad06@gmail.com>
2020-10-21 21:49:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/makeawishhw/AlarmReceiver.java
@@ -0,0 +1,27 @@
+package com.example.makeawishhw;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        deliverNotification(context);
+    }
+
+    private void deliverNotification(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+}
---
+++ app/src/main/java/com/example/makeawishhw/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.makeawishhw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Need NotificationManager
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Create Intent and PendingIntent
+
+        //Need AlarmManager
+
+        //Need ToggleButton
+
+
+    }
+}
